a4ca7cc48342bf6d4299e6319ea5e20fc53bae73
muhammadidzoha <muhammadidzoha@.com>
2024-05-23 23:09:27 +0700

    Add create ticket funnctionality

--- app/controllers/ticket.controller.js
+++ app/controllers/ticket.controller.js
@@ -23,7 +23,7 @@ exports.create = async (req, res) => {
     }
 
     const lastTicket = await prisma.ticketing.findFirst({
-      orderBy: { Nomor_TC: 'desc' },
+      orderBy: { Nomor_TC: "desc" },
     });
 
     let newTicketNumber = "001";
@@ -31,7 +31,7 @@ exports.create = async (req, res) => {
       const currentNumber = parseInt(lastTicket.Nomor_TC.slice(2));
       newTicketNumber = String(currentNumber + 1).padStart(3, "0");
     }
-    
+
     await prisma.ticketing.create({
       data: {
         UUID_TC: uuidv4(),
@@ -41,8 +41,8 @@ exports.create = async (req, res) => {
         isDone_TC: false,
         isCancelled_TC: false,
         isWaiting_TC: true,
-      }
-    })
+      },
+    });
 
     return res.status(200).json({
       message: "Ticket created",
@@ -50,14 +50,71 @@ exports.create = async (req, res) => {
   } catch (e) {
     return res.status(500).json({ error: "An error occured" + e });
   }
-}
+};
 
 exports.findAll = async (req, res) => {
   try {
-    const responseDatas = await prisma.ticketing.findMany({});
+    const responseDatas = await prisma.ticketing.findMany({
+      orderBy: { Nomor_TC: "desc" },
+    });
 
     return res.status(200).json(responseDatas);
   } catch (e) {
     return res.status(500).json({ error: "An error occured" + e });
   }
-}
+};
+
+exports.cancel = async (req, res) => {
+  try {
+    const ticketId = req.params.id;
+
+    if (!ticketId) {
+      return res.status(400).send({ message: "Missing ticket ID in request parameters" });
+    }
+
+    const updatedTicket = await prisma.ticketing.update({
+      where: { id: ticketId },
+      data: {
+        isWaiting_TC: false,
+        isCancelled_TC: true,
+      },
+    });
+
+    if (!updatedTicket) {
+      return res.status(404).send({ message: "Ticket not found" });
+    }
+
+    return res.status(200).json({ message: "Ticket cancelled successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "An error occurred while cancelling the ticket" });
+  }
+};
+
+exports.complete = async (req, res) => {
+  try {
+    const ticketId = req.params.id; // Assuming the ticket ID is in the request parameter 'id'
+
+    if (!ticketId) {
+      return res.status(400).send({ message: "Missing ticket ID in request parameters" });
+    }
+
+    const updatedTicket = await prisma.ticketing.update({
+      where: { id: ticketId },
+      data: {
+        isDone_TC: true,
+        isWaiting_TC: false,
+        isCancelled_TC: false,
+      },
+    });
+
+    if (!updatedTicket) {
+      return res.status(404).send({ message: "Ticket not found" });
+    }
+
+    return res.status(200).json({ message: "Ticket completed successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "An error occurred while completing the ticket" });
+  }
+};
+
--- app/routes/ticket.routes.js
+++ app/routes/ticket.routes.js
@@ -10,5 +10,15 @@ const ticket_controller = require("../controllers/ticket.controller");
 // ROUTER CONFIGURATION
 router.post("/ticket-management", authenticateToken, ticket_controller.create);
 router.get("/ticket-management", authenticateToken, ticket_controller.findAll);
+router.post(
+  "/ticket-management/complete/:uuid",
+  authenticateToken,
+  ticket_controller.complete
+);
+router.post(
+  "/ticket-management/cancel/:uuid",
+  authenticateToken,
+  ticket_controller.cancel
+);
 
 module.exports = router;
